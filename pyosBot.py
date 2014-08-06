5d1bbe5cb216648ad4a76ceb0a9a77c210bf7a60
Jay Turla <shipcodez@gmail.com>
2014-08-06 09:26:11 +0800

    Update pyosBot.py

--- pyosBot.py
+++ pyosBot.py
@@ -1,30 +1,36 @@
 import socket
 import os
 import sys
-import random
-from re import search
  
-network = 'irc.freenode.net'
+network = 'chat.freenode.net'
 port = 6667
 irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 irc.connect ( ( network, port ) )
 print irc.recv ( 4096 )
-irc.send ( 'NICK piratangcd\r\n' )
-irc.send ( 'USER jjt :just a fskin decoy\r\n' )
+irc.send ( 'NICK caloy\r\n' )
+irc.send ( 'USER jjt jjt jjt :just a fskin decoy\r\n' )
 irc.send ( 'JOIN #batibot\r\n' )
 irc.send ( 'PRIVMSG #batibot :Hello World.\r\n' )
-
 while True:
    data = irc.recv ( 4096 )
    if data.find ( 'PING' ) != -1:
       irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' )
-   if data.find ( '!botty quit' ) != -1:
+   if data.find ( '!quit' ) != -1:
       irc.send ( 'PRIVMSG #batibot :Fine, if you do not want me\r\n' )
       irc.send ( 'QUIT\r\n' )
    if data.find ( 'lol' ) != -1:
       irc.send ( 'PRIVMSG #batibot :Gay\r\n' )
-   if search ( '!command', data ):
-      data = data.split ( '!command' )
-      data = data[1].split ('\r\n')
-      command_msg = os.system(data[0])
-   print data
+   if data.find ( '!os') != -1:
+      a = data.split('!os')
+      a = a[1].split()
+      to = a[0]
+      if len[a] != 1:
+        irc.send('PRIVMSG #batibot :usage >> <!command> <command> \r\n' )
+      else:
+        os.system(to+" > /tmp/tempt.txt ")
+        a = open("/tmp/tempt.txt")
+        ot = a.read()
+        ot.replace("\r \n", "_")
+        a.close()
+        irc.send ('PRIVMSG #batibot :' +ot+ '\r\n')
+   print data 
